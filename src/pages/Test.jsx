ec264694b9732ee22887b83eb7e8282d711a3d39
redxzeta <xzetanzero@gmail.com>
2021-01-29 20:33:00 -0800

    nav stuff

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,3 +1,10 @@
-const Test = () => <h2>THIS IS A TEST PAGE</h2>;
+import Covex from "../assets/covex_logo.png";
+
+const Test = () => (
+  <div>
+    <img src={Covex} alt="covex" />
+    <h2>THIS IS A TEST PAGE</h2>
+  </div>
+);
 
 export default Test;
