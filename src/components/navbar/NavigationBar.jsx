88918b57525dcea9a76ce5857e6156c8ab170938
Alan <alan@remote.com>
2021-01-30 11:32:17 -0600

    format document

--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -3,27 +3,27 @@ import { Link } from "react-router-dom";
 
 import Logo from "../../assets/covax-needle.png";
 import "./nav.css";
-const NavigationBar = () => 
+const NavigationBar = () =>
 (
-    <Navbar bg="primary" expand="lg">
-      <Navbar.Brand as={Link} to="/">
-        <img src={Logo} alt="logo" className="logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/writeReview">
-            Write a review
+  <Navbar bg="primary" expand="lg">
+    <Navbar.Brand as={Link} to="/">
+      <img src={Logo} alt="logo" className="logo" />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link as={Link} to="/writeReview">
+          Write a review
           </Nav.Link>
-          <Nav.Link as={Link} to="/showReviews">
-            Show reviews
+        <Nav.Link as={Link} to="/showReviews">
+          Show reviews
           </Nav.Link>
-          <Nav.Link as={Link} to="/faq">
-            FAQ
+        <Nav.Link as={Link} to="/faq">
+          FAQ
           </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
 export default NavigationBar;
