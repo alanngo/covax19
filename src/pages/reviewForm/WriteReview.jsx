ef0c11c2d8cba355757d7fa4c24507a13a9f063d
Alan <alan@remote.com>
2021-01-30 14:25:46 -0600

    boiler plate for writing review

--- src/pages/reviewForm/WriteReview.jsx
+++ src/pages/reviewForm/WriteReview.jsx
@@ -1,10 +1,19 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+
 
 const WriteReview = () => 
 {
+  let [review, setReview] = useState(0)
+  const handleClick = (e) => 
+  {
+      // implement logic here
+  }
+
   return (
     <Fragment>
       <h2>Write a review</h2>
+      <Button onClick={handleClick}>Submit Review</Button>
     </Fragment>
   );
 };
