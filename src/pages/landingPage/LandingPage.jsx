2b856b357f6c7f004221c407cc18a354ddd0b10f
Alan <alan@remote.com>
2021-01-30 11:58:53 -0600

    added titles for homepage

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,10 +1,13 @@
 import FullPageContainer from "../../components/layout/FullPageContainer";
 import "./landing.css";
+const title = "Covax-19 is the hubspot for Covid vaccine reviews"
+const subtitle="You can update us with your post-vaccine symptons, read other reviews and explore our FAQ"
 const LandingPage = () => {
   return (
     <FullPageContainer>
       <div className="landing--panels panel-green"></div>
-      <div className="landing--panels panel-yeet"></div>
+      <div className="landing--panels panel-yeet">
+      </div>
     </FullPageContainer>
   );
 };
