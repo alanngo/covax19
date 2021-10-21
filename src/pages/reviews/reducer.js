296a70ef73f6d957a9da47670c5db244b62786ee
Alan <alan@remote.com>
2021-10-21 05:14:01 -0500

    expanded reviews by 3

--- src/pages/reviews/reducer.js
+++ src/pages/reviews/reducer.js
@@ -1,7 +1,7 @@
 export const indexReducer = (state, action) => {
     switch (action.type) {
-        case "increment": return state + 1
-        case "decrement": return state - 1
+        case "increment": return state + 3
+        case "decrement": return state - 3
         default: throw new Error("should never go here")
     }
 }
